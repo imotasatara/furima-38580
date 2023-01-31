202cd8d5650daf898ddbb068e67347b3cf3ce72b
imotasatara <satommy6@gmail.com>
2023-01-31 22:09:54 +0900

    ユーザーモデル単体テストの実装

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,7 +6,7 @@ FactoryBot.define do
 
     nickname              {Faker::Name.initials(number: 2)}
     email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(number: 2, min_length: 6)}
+    password              {"1a" + Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
     family_name_kanji     {person.family.kanji}
     first_name_kanji      {person.first.kanji}
