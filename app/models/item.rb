0d615b2a093ae1f54d2e5c46e6716f4c138ddab0
imotasatara <satommy6@gmail.com>
2023-02-04 15:13:05 +0900

    ActiveHashの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,14 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :delivery_day
+
+  validates :category_id, :status_id, :shipping_fee_id, :prefecture_id, :delivery_day_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :image, :item_name, :description, :price, presence: true
 end
