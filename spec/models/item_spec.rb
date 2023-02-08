0377e3b8cf94b72ead2241a64b4752bf705e32ea
imotasatara <satommy6@gmail.com>
2023-02-08 21:45:18 +0900

    Itemsモデルの単体テストコードの修正と追記

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Item, type: :model do
   end
 
   describe '商品出品機能' do
-    context '出品できな場合' do
+    context '出品できない場合' do
       it '商品画像が空では出品できない' do
         @item.image = nil
         @item.valid?
@@ -27,8 +27,18 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
+      it 'カテゴリーが"---"では保存できない' do
+        @item.category_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
       it '商品の状態が空では出品できない' do
         @item.status_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Status can't be blank")
+      end
+      it '商品の状態が"---"では保存できない' do
+        @item.status_id = '1'
         @item.valid?
         expect(@item.errors.full_messages).to include("Status can't be blank")
       end
@@ -37,8 +47,18 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping fee can't be blank")
       end
+      it '配送料の負担が"---"では保存できない' do
+        @item.shipping_fee_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping fee can't be blank")
+      end
       it '発送元の地域が空では出品できない' do
         @item.prefecture_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it '発送元の地域が"---"では保存できない' do
+        @item.prefecture_id = '1'
         @item.valid?
         expect(@item.errors.full_messages).to include("Prefecture can't be blank")
       end
@@ -47,13 +67,23 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Delivery day can't be blank")
       end
+      it '発送までの日数が"---"では保存できない' do
+        @item.delivery_day_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery day can't be blank")
+      end
       it '価格が空では出品できない' do
         @item.price = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
-      it '価格が¥300~9,999,999に収まっていないと出品できない' do
+      it '価格が¥300未満でないと出品できない' do
         @item.price = '200'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price is out of setting range')
+      end
+      it '価格が9_999_999を超えると出品できない' do
+        @item.price = '10,000,000'
         @item.valid?
         expect(@item.errors.full_messages).to include('Price is out of setting range')
       end
@@ -73,7 +103,7 @@ RSpec.describe Item, type: :model do
       it '商品画像、商品名、商品説明、カテゴリー、商品の状態、配送料の負担、発送元の地域、発送日数、価格が存在すれば出品できる' do
         expect(@item).to be_valid
       end
-      it '価格が¥300~9,999,999以内であれば出品できる' do
+      it '価格が¥300~9_999_999以内であれば出品できる' do
         @item.price = '1000'
         expect(@item).to be_valid
       end
