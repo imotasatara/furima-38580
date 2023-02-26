c3fcb787148427b3024a264cd5bafa5d36d6f538
imotasatara <satommy6@gmail.com>
2023-02-26 18:20:29 +0900

    ActiveHashの設定

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -2,4 +2,7 @@ class Sale < ApplicationRecord
   belongs_to :item
   belongs_to :user
   has_one :shipment
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
 end
