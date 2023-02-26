3927f9c11a44d0b9e2ad18e5d223b6e0d4b98826
imotasatara <satommy6@gmail.com>
2023-02-26 16:49:03 +0900

    formオブジェクトのバリデーション修正

--- app/models/sales_address.rb
+++ app/models/sales_address.rb
@@ -5,13 +5,13 @@ class SalesAddress
   with_options presence: true do
     validates :item, :user, :sale
     validates :postal_code,format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows(e.g. 123-4567)"}
-    validates :phone_number, numericality: {only_integer: true, \A0\d{9,10}\z, message: "is invalid. Input only number"}
+    validates :phone_number, numericality: {only_integer: true, length: {in: 10..11}, message: "is invalid. Input only number"}
     validates :town_name, :house_number
   end
   validates :prefecture_id, numericality: {other_than: 1}
 
   def save
-    sale = Sale.create(item: item.id, user: user.id)
+    sale = Sale.create(item: item_id, user: user_id)
     Shipment.create(postal_code: postal_code, prefecture_id: prefecture_id, town_name: town_name, house_number: house_number, building_name: building_name, phone_number: phone_number, sale: sale.id)
   end
 end
