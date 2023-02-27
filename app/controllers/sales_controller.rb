dedb07792a971608aaac8305b12116a7cbdce027
imotasatara <satommy6@gmail.com>
2023-02-27 15:55:13 +0900

    購入ページ表示、バリデーション修正と購入機能の実装

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,7 +1,28 @@
 class SalesController < ApplicationController
+  before_action :set_item
 
   def index
-    @item = Item.find(params[:item_id])
+    @sales_address = SalesAddress.new
   end
 
+  def create
+    @sales_address = SalesAddress.new(sales_params)
+    if @sales_address.valid?
+      @sales_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def sales_params
+    params.require(:sales_address).permit(:postal_code, :prefecture_id, :town_name, :house_number, :building_name, :phone_number, :sale_id,).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+  
 end
