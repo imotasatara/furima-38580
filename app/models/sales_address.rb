0baa0209dac30a27d845fedf05e0f53a14d92be8
imotasatara <satommy6@gmail.com>
2023-03-05 10:51:51 +0900

    単体テストコードの実装

--- app/models/sales_address.rb
+++ app/models/sales_address.rb
@@ -1,17 +1,19 @@
 class SalesAddress
   include ActiveModel::Model
-  attr_accessor :item_id, :user_id, :postal_code, :prefecture_id, :town_name, :house_number, :building_name, :phone_number, :sale_id
+  attr_accessor :item_id, :user_id, :postal_code, :prefecture_id, :town_name, :house_number, :building_name, :phone_number,
+                :sale_id
 
   with_options presence: true do
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows(e.g. 123-4567)"}
-    validates :phone_number, length: {in: 10..11, message: "should be 10 or 11 digits in length only"},
-      numericality: {only_integer: true, message: "is invalid. Input only number"}
-    validates :town_name, :house_number
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Enter it as follows(e.g. 123-4567)' }
+    validates :phone_number, length: { in: 10..11, message: 'should be 10 or 11 digits in length only' },
+                             numericality: { only_integer: true, message: 'is invalid. Input only number' }
+    validates :user_id, :town_name, :house_number
   end
-  validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 
   def save
     sale = Sale.create(item_id: item_id, user_id: user_id)
-    Shipment.create(postal_code: postal_code, prefecture_id: prefecture_id, town_name: town_name, house_number: house_number, building_name: building_name, phone_number: phone_number, sale_id: sale.id)
+    Shipment.create(postal_code: postal_code, prefecture_id: prefecture_id, town_name: town_name, house_number: house_number,
+                    building_name: building_name, phone_number: phone_number, sale_id: sale.id)
   end
 end
---
+++ spec/factories/sales_addresses.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :sales_address do
+    item_id { '1' }
+    user_id { '1' }
+    postal_code { '123-4567' }
+    prefecture_id { 2 }
+    town_name { '東京都' }
+    house_number { '1-1' }
+    building_name { '東京ハイツ' }
+    phone_number { '09011111111' }
+    sale_id { '1' }
+  end
+end
